0b93300530403b53f9934496cf30876d2bcd7d4f
mücahit bedir <mcbedir57@gmail.com>
2021-12-01 17:22:45 +0300

    Navigation Component ile sayfalar arası geçiş yapıldı.

--- app/src/main/java/com/mucahit_bedir/fizyoegzersiz/ui/authentication/LoginFragment.kt
+++ app/src/main/java/com/mucahit_bedir/fizyoegzersiz/ui/authentication/LoginFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.mucahit_bedir.fizyoegzersiz.R
 import com.mucahit_bedir.fizyoegzersiz.databinding.FragmentLoginBinding
 
 
-class LoginFragment : Fragment() {
+class LoginFragment : Fragment(), View.OnClickListener {
 
     private lateinit var binding: FragmentLoginBinding
 
@@ -20,6 +23,34 @@ class LoginFragment : Fragment() {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        listOf(
+            binding.girisButton,
+            binding.kayitButton,
+            binding.forgetPasswordTextView
+        ).forEach {
+            it.setOnClickListener(this)
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            binding.girisButton -> {
+
+            }
+            binding.kayitButton -> {
+                val action = LoginFragmentDirections.actionLoginFragmentToSignUpFragment()
+                findNavController().navigate(action)
+            }
+            binding.forgetPasswordTextView -> {
+                val action = LoginFragmentDirections.actionLoginFragmentToForgetPasswordFragment()
+                findNavController().navigate(action)
+            }
+        }
+
     }
 
 }
